fe65f3b95169addc4807185daa9c530eb4c1e638
Nick McLean <nickisyourfan@gmail.com>
2025-04-19 21:50:39 -0500

    auto close meu

--- client/components/navbar/index.tsx
+++ client/components/navbar/index.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Navbar as HeroUINavbar,
   NavbarContent,
@@ -16,73 +18,84 @@ import { siteConfig } from "@/config/site";
 import { ThemeSwitch } from "@/components/theme-switch";
 import { GithubIcon, Logo } from "@/components/icons";
 import { IdentifierDropdown } from "./components";
+import { useState } from "react";
 
-export const Navbar = () => (
-  <HeroUINavbar maxWidth="xl" position="sticky">
-    <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
-      <NavbarBrand as="li" className="gap-3 max-w-fit">
-        <NextLink className="flex justify-start items-center gap-1" href="/">
-          <Logo width={32} height={32} className="mr-6" />
-          <p className="font-bold text-inherit">RLY</p>
-        </NextLink>
-      </NavbarBrand>
-      <ul className="hidden md:flex gap-4 justify-start ml-2">
-        {siteConfig.navItems.map((item) => (
-          <NavbarItem key={item.href}>
-            <NextLink
-              className={clsx(
-                linkStyles({ color: "foreground" }),
-                "data-[active=true]:text-primary data-[active=true]:font-medium",
-              )}
-              color="foreground"
-              href={item.href}
-            >
-              {item.label}
-            </NextLink>
-          </NavbarItem>
-        ))}
-      </ul>
-    </NavbarContent>
+export const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    <NavbarContent className="hidden sm:flex basis-1/5 sm:basis-full" justify="end">
-      <NavbarItem className="hidden sm:flex gap-2">
+  return (
+    <HeroUINavbar
+      maxWidth="xl"
+      position="sticky"
+      isMenuOpen={isMenuOpen}
+      onMenuOpenChange={setIsMenuOpen}
+    >
+      <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
+        <NavbarBrand as="li" className="gap-3 max-w-fit">
+          <NextLink className="flex justify-start items-center gap-1" href="/">
+            <Logo width={32} height={32} className="mr-6" />
+            <p className="font-bold text-inherit">RLY</p>
+          </NextLink>
+        </NavbarBrand>
+        <ul className="hidden md:flex gap-4 justify-start ml-2">
+          {siteConfig.navItems.map((item) => (
+            <NavbarItem key={item.href}>
+              <NextLink
+                className={clsx(
+                  linkStyles({ color: "foreground" }),
+                  "data-[active=true]:text-primary data-[active=true]:font-medium",
+                )}
+                color="foreground"
+                href={item.href}
+              >
+                {item.label}
+              </NextLink>
+            </NavbarItem>
+          ))}
+        </ul>
+      </NavbarContent>
+
+      <NavbarContent className="hidden sm:flex basis-1/5 sm:basis-full" justify="end">
+        <NavbarItem className="hidden sm:flex gap-2">
+          <Link isExternal aria-label="Github" href={siteConfig.links.github}>
+            <GithubIcon className="text-default-500" />
+          </Link>
+          <ThemeSwitch />
+          <IdentifierDropdown />
+        </NavbarItem>
+      </NavbarContent>
+
+      <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
         <Link isExternal aria-label="Github" href={siteConfig.links.github}>
           <GithubIcon className="text-default-500" />
         </Link>
         <ThemeSwitch />
         <IdentifierDropdown />
-      </NavbarItem>
-    </NavbarContent>
+        <NavbarMenuToggle />
+      </NavbarContent>
 
-    <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-      <Link isExternal aria-label="Github" href={siteConfig.links.github}>
-        <GithubIcon className="text-default-500" />
-      </Link>
-      <ThemeSwitch />
-      <IdentifierDropdown />
-      <NavbarMenuToggle />
-    </NavbarContent>
-
-    <NavbarMenu>
-      <div className="mx-4 mt-2 flex flex-col gap-2">
-        {siteConfig.navMenuItems.map((item, index) => (
-          <NavbarMenuItem key={`${item}-${index}`}>
-            <Link
-              color={
-                index === 2
-                  ? "primary"
-                  : index === siteConfig.navMenuItems.length - 1
-                    ? "danger"
-                    : "foreground"
-              }
-              href={item.href}
-              size="lg"
-            >
-              {item.label}
-            </Link>
-          </NavbarMenuItem>
-        ))}
-      </div>
-    </NavbarMenu>
-  </HeroUINavbar>
-);
+      <NavbarMenu>
+        <div className="mx-4 mt-2 flex flex-col gap-2">
+          {siteConfig.navMenuItems.map((item, index) => (
+            <NavbarMenuItem key={`${item}-${index}`}>
+              <Link
+                color={
+                  index === 2
+                    ? "primary"
+                    : index === siteConfig.navMenuItems.length - 1
+                      ? "danger"
+                      : "foreground"
+                }
+                href={item.href}
+                size="lg"
+                onPress={() => setIsMenuOpen(false)}
+              >
+                {item.label}
+              </Link>
+            </NavbarMenuItem>
+          ))}
+        </div>
+      </NavbarMenu>
+    </HeroUINavbar>
+  );
+};
